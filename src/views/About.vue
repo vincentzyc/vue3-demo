ce1d7c50f8d3d341fa82d7426f8242c23a9f74d3
Vincentyun <vincentzyc@qq.com>
2020-04-23 09:45:43 +0800

    count demo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>state.count is: {{ state.count }}</h1>
+    <h2>state.double is: {{ state.double }}</h2>
+    <h2>count is: {{ count }}</h2>
+    <button @click="increment()">add</button>
   </div>
 </template>
+
+<script>
+import { reactive, ref, computed, watch } from 'vue'
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2)
+    })
+    const count = ref(0)
+
+    function increment() {
+      state.count++
+      count.value++
+    }
+
+    watch(
+      () => state.double,
+      val => { console.log(`count is ${val}`) }
+    )
+
+    return {
+      state,
+      count,
+      increment
+    }
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  publicPath: "./",
+	productionSourceMap: false,
+  chainWebpack: config => {
+    config.plugins.delete('prefetch')
+  },
+	devServer: {
+    port: 8010
+    // proxy: 'http://test-cms.jetmobo.com'
+    // proxy: 'http://cms.jetmobo.com'
+    // proxy: 'http://192.168.220.26:2023',
+	}
+}
