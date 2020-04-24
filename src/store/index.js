64e73594a540443e791ffab9e662ba2920909dfd
Vincentyun <vincentzyc@qq.com>
2020-04-24 09:16:33 +0800

    Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,12 @@ import Vuex from 'vuex'
 
 export default Vuex.createStore({
   state: {
+    storeTest: 'hello world'
   },
   mutations: {
+    setStoreTest(state, value) {
+      state.storeTest = value
+    }
   },
   actions: {
   },
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="about">
-    <h1>state.count is: {{ state.count }}</h1>
-    <h2>state.double is: {{ state.double }}</h2>
-    <h2>count is: {{ count }}</h2>
-    <button @click="increment()">add</button>
+    <div style="border:1px dashed #333;margin:10px;padding:10px">
+      <h1>state.count is: {{ state.count }}</h1>
+      <h2>state.double is: {{ state.double }}</h2>
+      <h2>count is: {{ count }}</h2>
+      <button @click="increment()">add</button>
+    </div>
+
+    <div style="border:1px dashed #333;margin:10px;padding:10px">
+      <h1>vuex state storeTest：</h1>
+      <h1>{{storeTest}}</h1>
+      <button @click="updateStoreTest()">updateStoreTest</button>
+    </div>
   </div>
 </template>
 
@@ -14,6 +22,19 @@ export default {
   setup() {
     const { ctx } = getCurrentInstance();  //ctx 当前组件的实例，类似2.x的this
     console.log(ctx)
+
+    // ctx.$router 是 Vue Router 实例，里面包含了 currentRoute 可以获取到当前的路由信息
+    console.log(ctx.$router.currentRoute.value)
+
+
+    // Vuex 的语法和 API 基本没变
+    const storeTest = computed(() => ctx.$store.state.storeTest)
+    const updateStoreTest = () => {
+      count.value++
+      ctx.$store.commit('setStoreTest', 'hello world' + count.value);
+      console.log(storeTest.value);
+    }
+
     const state = reactive({
       count: 0,
       double: computed(() => state.count * 2)
@@ -23,8 +44,6 @@ export default {
     const increment = () => {
       state.count++
       count.value++
-      // ctx.$router 是 Vue Router 实例，里面包含了 currentRoute 可以获取到当前的路由信息
-      console.log(ctx.$router.currentRoute.value)
     }
 
     watch(
@@ -35,7 +54,9 @@ export default {
     return {
       state,
       count,
-      increment
+      increment,
+      storeTest,
+      updateStoreTest
     }
   }
 }
