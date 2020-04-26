33894edd59654a86a46277412488597c65d4f1f0
Vincentyun <vincentzyc@qq.com>
2020-04-26 08:43:41 +0800

    props

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld :msg="msg" />
+    <button @click="updatePropsMsg()">update props msg</button>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  setup() {
+    const msg = ref("Welcome to Your Vue.js App")
+    // const vm = getCurrentInstance();
+    function updatePropsMsg() {
+      msg.value = msg.value + " ha";
+    }
+    return {
+      msg,
+      updatePropsMsg
+    }
   }
 }
 </script>
