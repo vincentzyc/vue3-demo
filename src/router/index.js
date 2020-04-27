0f6ed4ee0c9a1b300d7785005c469029528e746d
Vincentyun <vincentzyc@qq.com>
2020-04-27 08:59:59 +0800

    todo

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,28 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue'
 
 const routes = [
-{
-  path: '/',
-  name: 'Home',
-  component: Home
-},
-{
-  path: '/about',
-  name: 'About',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-}
+  {
+    path: "/",
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/todo',
+    name: 'TodoList',
+    component: () => import('../views/TodoList.vue')
+  }
 ]
 
 const router = createRouter({
