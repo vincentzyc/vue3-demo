e17663e685ac70a4da88f3e8f906ad2e6df07659
Vincentyun <vincentzyc@qq.com>
2020-07-29 15:46:16 +0800

    init: add ts

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/todo">ToDo</router-link> |
-      <router-link to="/luckdraw">LuckDraw</router-link>
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>|
+      <!-- <router-link to="/todo">ToDo</router-link>|
+      <router-link to="/luckdraw">LuckDraw</router-link> -->
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App"
+});
+</script>
+
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
