aa04d990bde216eb3245337d525bef404e340c76
Vincentyun <vincentzyc@qq.com>
2020-07-29 15:43:59 +0800

    init : add ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { createApp } from 'vue';
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
