18e66a67d5f7db3cd2c32ef480cb44083f187e66
Vincentyun <vincentzyc@qq.com>
2020-07-30 09:05:21 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld :msg="msg" />
-    <button @click="updatePropsMsg()">update props msg</button>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import { ref } from 'vue'
 
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
   },
-  setup() {
-    const msg = ref("Welcome to Your Vue.js App")
-    const updatePropsMsg = () => {
-      msg.value = msg.value + " ha";
-    }
-    return {
-      msg,
-      updatePropsMsg
-    }
-  }
-}
+});
 </script>
