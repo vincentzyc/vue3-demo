341bb6c4d0d5792e15a423a0892faa5fe8edc8d6
Vincentyun <vincentzyc@qq.com>
2020-07-30 09:02:14 +0800

    ts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,33 +4,30 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from 'vue';
 
-export default {
-  name: "HelloWorld",
+export default defineComponent({
+  name: 'HelloWorld',
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-h3 {
-  margin: 40px 0 0;
-}
+h3
+  margin 40px 0 0
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+ul
+  list-style-type none
+  padding 0
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+li
+  display inline-block
+  margin 0 10px
 
-a {
-  color: #42b983;
-}
+a
+  color #42b983
 </style>
