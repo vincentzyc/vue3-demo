0767a78152453e4ea250f5fd237b92487af8af7d
Vincentyun <vincentzyc@qq.com>
2020-08-01 13:55:09 +0800

    ts

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div ref="root">
+    <p>This is a root element</p>
+    <input ref="todoinput" v-model="model" />
   </div>
 </template>
+
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+  setup() {
+    const root = ref(null)
+    const todoinput = ref(null)
+    const model = ref('')
+
+
+    onMounted(() => {
+      console.log(root.value)
+      todoinput.value.focus()
+    })
+
+    return {
+      root,
+      todoinput,
+      model
+    }
+  }
+}
+</script>
