b6a84653a0e9d9e027b27372ca68384226c780cd
Vincentyun <vincentzyc@qq.com>
2020-08-23 21:57:35 +0800

    utils

---
+++ src/utils/storage.ts
@@ -0,0 +1,23 @@
+export function setSessionStorage(key: string, value: any): void {
+  let str = window.JSON.stringify(value);
+  window.sessionStorage.setItem(key, str);
+}
+
+export function getSessionStorage(key: string) {
+  let json: string | null;
+  json = window.sessionStorage.getItem(key);
+  if (json) json = window.JSON.parse(json);
+  return json;
+}
+
+export function setLocalStorage(key: string, value: any): void {
+  let str = window.JSON.stringify(value);
+  window.localStorage.setItem(key, str);
+}
+
+export function getLocalStorage(key: string) {
+  let json: string | null;
+  json = window.localStorage.getItem(key);
+  if (json) json = window.JSON.parse(json);
+  return json;
+}
