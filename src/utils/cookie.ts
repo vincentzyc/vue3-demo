850e81f76b2adbae46dfe649e2e563203717683d
Vincentyun <vincentzyc@qq.com>
2020-08-23 21:57:14 +0800

    utils

---
+++ src/utils/cookie.ts
@@ -0,0 +1,22 @@
+export function setCookie(key: string, value: string, time: number): void {
+  let cur = new Date();
+  cur.setTime(cur.getTime() + time * 24 * 3600 * 1000);
+  document.cookie = `${key}=${encodeURIComponent(value)};expires=${time ? cur.toUTCString() : ''}}`
+}
+
+export function getCookie(key: string): string {
+  let data = document.cookie;
+  let startIndex = data.indexOf(key + '=');
+  if (startIndex > -1) {
+    startIndex = startIndex + key.length + 1;
+    let endIndex = data.indexOf(';', startIndex);
+    endIndex = endIndex < 0 ? data.length : endIndex;
+    return decodeURIComponent(data.substring(startIndex, endIndex));
+  } else {
+    return '';
+  }
+}
+
+export function delCookie(key: string): void {
+  setCookie(key, '', -1);
+}
