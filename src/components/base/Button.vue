4b4795ff4f1d1fda9edade0c9159a9126e842c24
Vincentyun <vincentzyc@qq.com>
2020-08-27 09:06:38 +0800

    components

---
+++ src/components/base/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <button>123</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "YuiButton",
+});
+</script>
