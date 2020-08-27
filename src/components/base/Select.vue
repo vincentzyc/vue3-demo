0a05854635a7448b94205f9d131b613558bd4eb1
Vincentyun <vincentzyc@qq.com>
2020-08-27 09:06:43 +0800

    components

---
+++ src/components/base/Select.vue
@@ -0,0 +1,16 @@
+<template>
+  <select name="123" id="abc">
+    <option value="1">haha</option>
+    <option value="a">aaa</option>
+    <option value="b">bbb</option>
+    <option value="c">ccc</option>
+  </select>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "YuiSelect",
+});
+</script>
