b5db43320e571b6eedf54403e1fa0312bdbbacf7
Vincentyun <vincentzyc@qq.com>
2020-08-27 09:07:46 +0800

    global components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <p>以下是自定义全局组件</p>
+    <div style="margin-bottom:20px">
+      <YuiButton></YuiButton>
+    </div>
+    <div style="margin-bottom:20px">
+      <YuiSelect></YuiSelect>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
     HelloWorld,
   },
