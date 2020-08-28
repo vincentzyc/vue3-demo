4b6ff06de39bce0cac7eeb5f424fc571e8dfb2b7
Vincentyun <vincentzyc@qq.com>
2020-08-28 09:15:19 +0800

    代码优化

--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -2,14 +2,9 @@ import { App } from 'vue'
 import Button from './Button.vue';
 import Select from './Select.vue';
 
-const version = '1.0';
-
 const install = function (app: App<Element>) {
   app.component('YuiButton', Button)
   app.component('YuiSelect', Select)
 }
 
-export default {
-  install,
-  version
-};
+export default install
