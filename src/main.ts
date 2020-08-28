139dbf2aac3db64d55e2919d5ee19f9424b09033
Vincentyun <vincentzyc@qq.com>
2020-08-28 09:15:42 +0800

    custom directive

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import BaseComponents from './components/base'
+import BaseDirective from './directive'
 
 const app = createApp(App)
 
 app.use(BaseComponents)
+app.use(BaseDirective)
 
 app.use(store).use(router).mount('#app')
