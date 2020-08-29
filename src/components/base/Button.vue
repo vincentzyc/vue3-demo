ac08f87174678788c8aaa7a1f097d0b39bd6b990
Vincentyun <vincentzyc@qq.com>
2020-08-29 09:51:41 +0800

    style

--- src/components/base/Button.vue
+++ src/components/base/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button>
+  <button class="yui-button">
     <slot>按钮</slot>
   </button>
 </template>
@@ -11,3 +11,25 @@ export default defineComponent({
   name: "YuiButton",
 });
 </script>
+
+<style lang="stylus" scoped>
+.yui-button {
+  border: none;
+  outline: 0;
+  padding: 8px 12px;
+  border-radius: 5px;
+  color: #FFF;
+  background-color: #409EFF;
+  display: inline-block;
+  line-height: 1;
+  white-space: nowrap;
+  cursor: pointer;
+  text-align: center;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  margin: 0;
+  -webkit-transition: .1s;
+  transition: .1s;
+  font-weight: 500;
+}
+</style>
