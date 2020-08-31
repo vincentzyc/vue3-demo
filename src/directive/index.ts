fbf0ef239cfb3663dcb56ed73d5f6515119e8a0e
Vincentyun <vincentzyc@qq.com>
2020-08-31 14:38:42 +0800

    custom directive

--- src/directive/index.ts
+++ src/directive/index.ts
@@ -1,5 +1,5 @@
 import { App } from 'vue'
-import focus from './focus';
+import focus from './focus'
 
 const install = function (app: App<Element>) {
   // Register a global custom directive called `v-focus`
