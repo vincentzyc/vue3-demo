ad967f92e7d1ee30baa5e0362b96dd109af16dca
Vincentyun <vincentzyc@qq.com>
2020-09-08 09:45:09 +0800

    样式优化

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/css/base.css'
+import './assets/css/main.styl'
 import BaseComponents from './components/base'
 import BaseDirective from './directive'
 
