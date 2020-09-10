97f16524d76779e6e408df3afbb49f8a767f571a
Vincentyun <vincentzyc@qq.com>
2020-09-10 09:32:00 +0800

    样式优化

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style lang="stylus">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.max640{
-  max-width 640px;
-  margin auto
-}
-</style>
