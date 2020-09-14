25f44013f7492ed802737b53f2b7764282141514
Vincentyun <vincentzyc@qq.com>
2020-09-14 16:27:53 +0800

    ts-import-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,35 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const tsImportPluginFactory = require('ts-import-plugin')
+
 module.exports = {
   publicPath: "./",
   outputDir: "docs",
-	productionSourceMap: false,
+  productionSourceMap: false,
   chainWebpack: config => {
     config.plugins.delete('prefetch')
+    config.module
+      .rule("ts")
+      .use("ts-loader")
+      .tap(options => {
+        options = Object.assign(options, {
+          transpileOnly: true,
+          getCustomTransformers: () => ({
+            before: [
+              tsImportPluginFactory({
+                libraryName: "vant",
+                libraryDirectory: "es",
+                style: true
+              })
+            ]
+          }),
+          compilerOptions: {
+            module: "es2015"
+          }
+        });
+        return options;
+      });
   },
-	devServer: {
+  devServer: {
     port: 8010
-	}
+  }
 }
