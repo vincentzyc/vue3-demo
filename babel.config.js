ed45c4b21eb52a944d203cb767fdab969c3cc939
Vincentyun <vincentzyc@qq.com>
2020-09-14 16:28:11 +0800

    init

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  plugins: [
-    ['import', {
-      libraryName: 'vant',
-      libraryDirectory: 'es',
-      style: true
-    }, 'vant']
   ]
 }
