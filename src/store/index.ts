53b415decd2be7bb1ef2c4e4f2060c8c9e151ebb
Vincentyun <vincentzyc@qq.com>
2020-09-21 11:15:12 +0800

    init

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,8 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    storeTest: 'hello world'
   },
   mutations: {
-    setStoreTest(state, value) {
-      state.storeTest = value
-    }
   },
   actions: {
   },
