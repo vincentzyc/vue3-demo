aff2c451b51e624dd51437184a1199662009ff65
Vincentyun <vincentzyc@qq.com>
2020-09-22 09:41:16 +0800

    vant

--- src/views/About.vue
+++ src/views/About.vue
@@ -17,16 +17,31 @@
     </div>
     <p class="mg-t10">{{formData}}</p>
     <Button type="primary" @click="insertName()">姓名插入哈哈</Button>
-    <Steps :active="1">
+    <!-- <Steps :active="1">
       <Step>买家下单</Step>
       <Step>商家接单</Step>
       <Step>买家提货</Step>
       <Step>交易完成</Step>
+    </Steps>-->
+    <Steps direction="vertical" :active="1">
+      <Step>
+        <h3>【城市】物流状态1</h3>
+        <p>{{twoNow}}</p>
+      </Step>
+      <Step>
+        <h3>【城市】物流状态2</h3>
+        <p>{{now}}</p>
+      </Step>
+      <Step>
+        <h3>快件已发货</h3>
+        <p>{{now2}}</p>
+      </Step>
     </Steps>
   </div>
 </template>
 
 <script lang="ts">
+import dayjs from "dayjs";
 import { ref, reactive, onMounted } from "vue";
 import { Button, Step, Steps } from "vant";
 
@@ -39,6 +54,9 @@ export default {
   setup() {
     const nameinput = ref();
     const selectionStart = ref(0);
+    const twoNow = dayjs().subtract(2, "day").format("YYYY-MM-DD HH:mm:ss");
+    const now = dayjs().format("YYYY-MM-DD HH:mm:ss");
+    const now2 = dayjs().add(2, "day").format("YYYY-MM-DD HH:mm:ss");
     const formData = reactive({
       name: "",
       phone: "",
@@ -61,6 +79,9 @@ export default {
       formData,
       insertName,
       selectionStart,
+      twoNow,
+      now,
+      now2,
     };
   },
 };
