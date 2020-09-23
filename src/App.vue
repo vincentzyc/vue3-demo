af02bdf85e4990da7cfaa579c30ab7782c84185e
Vincentyun <vincentzyc@qq.com>
2020-09-23 14:03:00 +0800

    vant

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="wrapper">
-    <YunFooter></YunFooter>
     <transition name="van-fade">
       <router-view />
     </transition>
+    <Tabbar route placeholder>
+      <TabbarItem replace to="/" icon="home-o">Home</TabbarItem>
+      <TabbarItem replace to="/about" icon="search">About</TabbarItem>
+      <TabbarItem replace to="/todoList" icon="friends-o">ToDo</TabbarItem>
+      <TabbarItem replace to="/luckdraw" icon="setting-o">LuckDraw</TabbarItem>
+      <TabbarItem replace to="/tsx" icon="star-o">About</TabbarItem>
+    </Tabbar>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import YunFooter from "@/components/YunFooter.vue";
+import { Tabbar, TabbarItem } from "vant";
 
 export default defineComponent({
   name: "App",
   components: {
-    YunFooter,
+    Tabbar,
+    TabbarItem,
   },
 });
 </script>
