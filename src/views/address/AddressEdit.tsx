a3f44b2b867dc329c3846b95e82bb651cf1d67d9
Vincentyun <vincentzyc@qq.com>
2020-09-28 12:12:16 +0800

    address

--- src/views/address/AddressEdit.tsx
+++ src/views/address/AddressEdit.tsx
@@ -1,6 +1,6 @@
-import { ref } from "vue";
-import { defineComponent } from 'vue';
-import { Field } from "vant";
+import { ref, defineComponent } from "vue";
+import { Field, Cell } from "vant";
+import CityPicker from "@/components/city-picker"
 
 export default defineComponent({
   setup() {
@@ -9,41 +9,28 @@ export default defineComponent({
     const digit = ref('')
     const number = ref('')
     const password = ref('')
-    // const onSave = () => {
-    //   Toast('save')
-    // }
-    // const onDelete = () => {
-    //   Toast('delete')
-    // }
-    // let searchResult: {
-    //   address: string;
-    //   name: string;
-    // }[] = reactive([])
+    const city = ref(['', '', ''])
+    const cityPicker = ref()
 
-    // const onChangeDetail = (val: any) => {
-    //   if (val) {
-    //     searchResult = [
-    //       {
-    //         name: '黄龙万科中心',
-    //         address: '杭州市西湖区',
-    //       },
-    //     ]
-    //   } else {
-    //     searchResult = [];
-    //   }
-    // }
+    const openCity = () => {
+      console.log(cityPicker.value);
+      cityPicker.value.open()
+    }
     return () => (
       <div class="mg10">
         {/* <!-- 输入任意文本 --> */}
-        <Field vModel={text.value} label="文本" placeholder="请输入文本" />
+        <Field v-model={text.value} label="文本" placeholder="请输入文本" />
         {/* <!-- 输入手机号，调起手机号键盘 --> */}
-        <Field vModel={tel.value} type="tel" label="手机号" placeholder="请输入手机号" />
+        <Field v-model={tel.value} type="tel" label="手机号" placeholder="请输入手机号" />
         {/* <!-- 允许输入正整数，调起纯数字键盘 --> */}
-        <Field vModel={digit.value} type="digit" label="整数" placeholder="请输入整数" />
+        <Field v-model={digit.value} type="digit" label="整数" placeholder="请输入整数" />
         {/* <!-- 允许输入数字，调起带符号的纯数字键盘 --> */}
-        <Field vModel={number.value} type="number" label="数字" placeholder="请输入数字（支持小数）" />
+        <Field v-model={number.value} type="number" label="数字" placeholder="请输入数字（支持小数）" />
         {/* <!-- 输入密码 --> */}
-        <Field vModel={password.value} type="password" label="密码" placeholder="请输入密码" />
+        <Field v-model={password.value} type="password" label="密码" placeholder="请输入密码" />
+
+        <Cell is-link onClick={openCity}>选择城市</Cell>
+        <CityPicker v-model={city.value} ref={cityPicker} />
       </div>
     )
   }
