ebc2524d5ef18ce1d4e5bf1794ed8148fe532068
Vincentyun <vincentzyc@qq.com>
2020-10-02 09:28:17 +0800

    address

--- src/views/address/AddressEdit.tsx
+++ src/views/address/AddressEdit.tsx
@@ -1,26 +1,56 @@
-import { ref, defineComponent } from "vue";
-import { Field, Cell } from "vant";
+import { defineComponent, ref, computed } from "vue";
+import { Field, NavBar } from "vant";
+import { useRouter } from "vue-router";
 import CityPicker from "@/components/city-picker"
 
 export default defineComponent({
   setup() {
+    const router = useRouter()
+
     const text = ref('')
     const tel = ref('')
     const address = ref('')
-    const city = ref(['', '', ''])
+    const city = ref([])
     const cityPicker = ref()
 
+    const cityValue = computed(() => {
+      if (city.value.length > 0) return city.value.join(' ')
+      return ''
+    })
+
     const openCity = () => {
-      console.log(cityPicker.value);
       cityPicker.value.open()
     }
+
+    const onClickLeft = () => {
+      router.back()
+    }
+
+
     return () => (
-      <div class="mg10">
-        <Field v-model={text.value} label="姓名" placeholder="请输入姓名" />
-        <Field v-model={tel.value} type="tel" label="手机号" placeholder="请输入手机号" />
-        <Cell is-link onClick={openCity}>选择城市</Cell>
+      <div>
+        <NavBar
+          title="地址管理"
+          left-text="返回"
+          left-arrow
+          onClick-left={onClickLeft}
+        />
+        <div class="mg10">
+          <Field v-model={text.value} label="姓名" placeholder="请输入姓名" />
+          <Field v-model={tel.value} type="tel" label="手机号" placeholder="请输入手机号" />
+          <Field
+            readonly
+            label="选择城市"
+            rightIcon="arrow"
+            modelValue={cityValue.value}
+            placeholder="请选择城市"
+            onClick={openCity}
+          />
+          <Field v-model={address.value} label="详细地址" placeholder="请输入详细地址" />
+        </div>
+
         <CityPicker v-model={city.value} ref={cityPicker} />
-        <Field v-model={address.value} label="详细地址" placeholder="请输入详细地址" />
+
       </div>
     )
   }
