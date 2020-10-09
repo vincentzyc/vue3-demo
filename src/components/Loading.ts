dc4b9409833053ce6a4b381bf0e404766bd651de
Vincentyun <vincentzyc@qq.com>
2020-10-09 10:13:48 +0800

    loading

--- src/components/Loading.ts
+++ src/components/Loading.ts
@@ -6,6 +6,7 @@ export function openLoading(text: string) {
     duration: 0,       // 持续展示 toast
     forbidClick: true, // 禁用背景点击
     overlay: true,
+    loadingType: 'spinner'  //默认 circular
   });
 }
 
