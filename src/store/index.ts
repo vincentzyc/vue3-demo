6ea6479940e1156196673b379b9c3159cb396f33
Vincentyun <vincentzyc@qq.com>
2020-10-10 09:06:59 +0800

    store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,8 +2,12 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    selectAddress: null
   },
   mutations: {
+    setSelectAddress(state, payload) {
+      state.selectAddress = payload
+    }
   },
   actions: {
   },
