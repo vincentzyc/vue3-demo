c78468f5b8324c6910be48ddb8860ef6d9f27953
Vincentyun <vincentzyc@qq.com>
2020-10-21 10:02:03 +0800

    easeBottom

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,33 +1,24 @@
 /**
- * 滑动到指定位置的缓冲动画函数
- * @param {Object} dom 目标dom
- * @param {Number} destination 目标位置
- * @param {Number} rate 缓动率(越大速度越慢)
- * 示例用法
-   let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-   let dom = scrollTop === document.documentElement.scrollTop ? document.documentElement : document.body;
-   this.$util.easeout(dom, 0);
+ * 滑动到底部
  */
-export function easeout(dom: Element, destination = 0, rate = 5) {
-  let position = dom.scrollTop;
-  if (position === destination || typeof destination !== "number" || rate === 0) {
-    return false;
-  }
+export function easeBottom() {
+  let position = window.pageYOffset
+  const destination = document.documentElement.offsetHeight - document.documentElement.clientHeight
   // 不存在原生`requestAnimationFrame`，用`setTimeout`模拟替代
   if (!window.requestAnimationFrame) {
     window.requestAnimationFrame = function (fn) {
       return setTimeout(fn, 17);
     };
   }
-  const step = function () {
-    position = position + (destination - position) / rate;
+  function step() {
+    position = position + (destination - position) / 8;
     if (Math.abs(destination - position) < 2) {
       //动画结束
-      dom.scrollTop = destination;
+      window.scrollTo(0, destination)
       return;
     }
-    dom.scrollTop = position;
+    window.scrollTo(0, position)
     requestAnimationFrame(step);
-  };
+  }
   step();
 }
