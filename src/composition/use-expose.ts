fa12cb44bbd9685800e311c56d7ae87781271f7b
vincentzyc <vincentzyc@qq.com>
2021-08-11 15:08:07 +0800

    ⚡perf: 代码优化

--- src/composition/use-expose.ts
+++ src/composition/use-expose.ts
@@ -1,7 +1,7 @@
 import { getCurrentInstance } from 'vue';
 
 // expose public api
-export function useExpose(apis: Record<string, any>) {
+export function useExpose(apis: Record<string, any>): void {
   const instance = getCurrentInstance();
   if (instance) {
     Object.assign(instance.proxy, apis);
