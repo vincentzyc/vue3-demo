4dc25e7b4c0e7d856075104f1e43167fafe94bc7
vincentzyc <vincentzyc@qq.com>
2021-08-11 15:05:51 +0800

    🐳chore: add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
   }
 }
