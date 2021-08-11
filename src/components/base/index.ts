f4bf772ceac2b6c88e8424cc2fee6be67b6910fd
vincentzyc <vincentzyc@qq.com>
2021-08-11 15:07:59 +0800

    ⚡perf: 代码优化

--- src/components/base/index.ts
+++ src/components/base/index.ts
@@ -2,7 +2,7 @@ import { App } from 'vue'
 import Button from './Button.vue';
 import Select from './Select.vue';
 
-const install = function (app: App<Element>) {
+const install = function (app: App<Element>): void {
   app.component('YuiButton', Button)
   app.component('YuiSelect', Select)
 }
