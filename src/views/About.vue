b71ac6199877dd8ff02dd021fb1257959f844aeb
vincentzyc <vincentzyc@qq.com>
2021-08-13 10:23:43 +0800

    ✨ feat: script setup

--- src/views/About.vue
+++ src/views/About.vue
@@ -16,69 +16,48 @@
         <input type="tel" v-model.trim="formData.code" maxlength="6" />
       </div>
     </div>
-    <p class="mg-t10">{{formData}}</p>
+    <p class="mg-t10">{{ formData }}</p>
     <Button type="primary" @click="insertName()">姓名插入哈哈</Button>
     <Steps direction="vertical" :active="1">
       <Step>
         <h3>商品已下单</h3>
-        <p>{{twoNow}}</p>
+        <p>{{ twoNow }}</p>
       </Step>
       <Step>
         <h3>快件已被揽收</h3>
-        <p>{{now}}</p>
+        <p>{{ now }}</p>
       </Step>
       <Step>
         <h3>快件已发货</h3>
-        <p>{{now2}}</p>
+        <p>{{ now2 }}</p>
       </Step>
     </Steps>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import dayjs from "dayjs";
 import { ref, reactive, onMounted } from "vue";
 import { Button, Step, Steps, NoticeBar } from "vant";
 
-export default {
-  components: {
-    Button,
-    Step,
-    Steps,
-    NoticeBar,
-  },
-  setup() {
-    const nameinput = ref();
-    const selectionStart = ref(0);
-    const twoNow = dayjs().subtract(2, "day").format("YYYY-MM-DD HH:mm:ss");
-    const now = dayjs().format("YYYY-MM-DD HH:mm:ss");
-    const now2 = dayjs().add(2, "day").format("YYYY-MM-DD HH:mm:ss");
-    const formData = reactive({
-      name: "",
-      phone: "",
-      code: "",
-    });
+const nameinput = ref();
+const twoNow = dayjs().subtract(2, "day").format("YYYY-MM-DD HH:mm:ss");
+const now = dayjs().format("YYYY-MM-DD HH:mm:ss");
+const now2 = dayjs().add(2, "day").format("YYYY-MM-DD HH:mm:ss");
+const formData = reactive({
+  name: "",
+  phone: "",
+  code: "",
+});
 
-    onMounted(() => {
-      (nameinput.value as HTMLInputElement).focus();
-    });
+onMounted(() => {
+  (nameinput.value as HTMLInputElement).focus();
+});
 
-    const insertName = () => {
-      const index = (nameinput.value as HTMLInputElement).selectionStart;
-      if (typeof index !== "number") return;
-      formData.name =
-        formData.name.slice(0, index) + "哈哈" + formData.name.slice(index);
-    };
-
-    return {
-      nameinput,
-      formData,
-      insertName,
-      selectionStart,
-      twoNow,
-      now,
-      now2,
-    };
-  },
-};
+const insertName = () => {
+  const index = (nameinput.value as HTMLInputElement).selectionStart;
+  if (typeof index !== "number") return;
+  formData.name =
+    formData.name.slice(0, index) + "哈哈" + formData.name.slice(index);
+}
 </script>
