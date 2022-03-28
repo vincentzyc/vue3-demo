decbde264945e061bb23567b73dc600f349defe6
vincentzyc <vincentzyc@qq.com>
2022-03-28 15:02:13 +0800

    🌈 style: 新增过度动画

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue')
+    component: () => import(/* webpackChunkName: "home" */ '@/views/HomeView.vue')
   },
   {
     path: '/about',
@@ -12,7 +12,7 @@ const routes: Array<RouteRecordRaw> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '@/views/AboutView.vue')
   },
   {
     path: '/todoList',
