84751ff7bc3f1aed193c0f5a9d0d9e3e4ca9f377
vincentzyc <vincentzyc@qq.com>
2022-03-29 08:47:16 +0800

    ✨ feat: add pinia

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,13 @@ import './assets/css/base.css'
 import './assets/css/main.styl'
 import BaseComponents from './components/base'
 import BaseDirective from './directive'
+import { createPinia } from 'pinia'
+
 
 const app = createApp(App)
 
 app.use(BaseComponents)
 app.use(BaseDirective)
+app.use(createPinia())
 
 app.use(store).use(router).mount('#app')
