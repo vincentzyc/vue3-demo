f3e086150abfb48784f2a3fec3bb57f419760511
vincentzyc <vincentzyc@qq.com>
2023-05-22 06:59:01 +0800

    🐳chore: change vite

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/css/base.css'
-import './assets/css/main.styl'
+import './assets/css/main.less'
 import BaseComponents from './components/base'
 import BaseDirective from './directive'
 import { createPinia } from 'pinia'
+import 'vant/es/dialog/style';
+import 'vant/es/toast/style';
 
 
 const app = createApp(App)
