96ef70176da2d71a9f14a64fc1c4876b5cd33070
vincentzyc <vincentzyc@qq.com>
2023-05-22 06:58:54 +0800

    🐳chore: change vite

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -3,47 +3,46 @@
     <h3 class="mg10">Todo List</h3>
     <div class="flex flex-center">
       <input ref="todoinput" v-focus="200" v-model="addTodoName" />
-      <Button @click="addTodoAction" size="small" type="primary">新增清单</Button>
+      <van-button @click="addTodoAction" size="small" type="primary">新增清单</van-button>
     </div>
     <div>
       <h3 class="mg10">任务清单</h3>
-      <CellGroup>
-        <Cell :key="item.id" v-for="item in undoneTodoList">
+      <van-cell-group>
+        <van-cell :key="item.id" v-for="item in undoneTodoList">
           <template #title>
             <span>---{{ item.name }}---</span>
           </template>
           <template #right-icon>
-            <Button @click="doneTodo(item)" size="small" type="success">已完成</Button>
-            <Button @click="delTodoAction(item, true)" size="small" type="danger">删除</Button>
+            <van-button @click="doneTodo(item)" size="small" type="success">已完成</van-button>
+            <van-button @click="delTodoAction(item, true)" size="small" type="danger">删除</van-button>
           </template>
-        </Cell>
-      </CellGroup>
+        </van-cell>
+      </van-cell-group>
     </div>
     <div class="done-todo-area">
       <h3 class="mg10">已完成的任务清单</h3>
-      <CellGroup>
-        <Cell :key="item.id" v-for="item in completedTodoList">
+      <van-cell-group>
+        <van-cell :key="item.id" v-for="item in completedTodoList">
           <template #title>
             <span>---{{ item.name }}---</span>
           </template>
           <template #right-icon>
-            <Button @click="delTodoAction(item, false)" size="small" type="danger">删除</Button>
+            <van-button @click="delTodoAction(item, false)" size="small" type="danger">删除</van-button>
           </template>
-        </Cell>
-      </CellGroup>
+        </van-cell>
+      </van-cell-group>
     </div>
     <div class="mg10">
-      <Button @click="goAddress" block type="primary">地址列表</Button>
+      <van-button @click="goAddress" block type="primary">地址列表</van-button>
     </div>
     <div class="mg10 mg-t20">
-      <Button @click="goChat" block type="primary">聊天室</Button>
+      <van-button @click="goChat" block type="primary">聊天室</van-button>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref, reactive, getCurrentInstance } from "vue";
-import { Button, Cell, CellGroup } from "vant";
 import { useRouter } from 'vue-router'
 import { useMainStore } from '@/pinia'
 
