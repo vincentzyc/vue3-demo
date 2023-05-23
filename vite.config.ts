5595361e5512d984c00d03090cf08020ea44c0af
vincentzyc <vincentzyc@qq.com>
2023-05-23 14:30:25 +0800

    🐳chore: change vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,7 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    vueJsx(),
     Components({
       resolvers: [VantResolver()],
     }),
