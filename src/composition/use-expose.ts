1ba26c4e2210f7b588a7d6ebc957249d2b399548
vincentzyc <vincentzyc@qq.com>
2023-05-23 14:29:44 +0800

    🐳chore: change vite

--- src/composition/use-expose.ts
+++
@@ -1,9 +0,0 @@
-import { getCurrentInstance } from 'vue';
-
-// expose public api
-export function useExpose(apis: Record<string, any>): void {
-  const instance = getCurrentInstance();
-  if (instance) {
-    Object.assign(instance.proxy, apis);
-  }
-}
