849f2f61d15f431d7baadddaa7e39f76f89a9663
vincentzyc <vincentzyc@qq.com>
2023-05-25 09:19:30 +0800

    🐳chore: change vite

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,19 @@
-interface Window {
-  scroll2Bottom (): void
-  // dayjs: any;
+declare global {
+  interface Window {
+    scroll2Bottom(): void;
+  }
+}
+declare module '@vant/area-data' {
+  export declare const areaList: {
+    province_list: Record<string, string>;
+    city_list: Record<string, string>;
+    county_list: Record<string, string>;
+  };
+  type CascaderOption = {
+    text: string;
+    value: string;
+    children?: CascaderOption[];
+  };
+  export declare function useCascaderAreaData(): CascaderOption[];
+  export {};
 }
